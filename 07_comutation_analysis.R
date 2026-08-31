102a171deabe5861ad563bb2fc9f1e8a83da8725
Ras Preeti Sharma <raspreeti09@gmail.com>
2026-08-31 13:21:00 +0530

    Update 07_comutation_analysis.R

--- 07_comutation_analysis.R
+++ 07_comutation_analysis.R
@@ -8,7 +8,7 @@
 #   D. Clonality / VAF-based clonal hierarchy inference
 # =============================================================================
 
-source("c:/Users/asus/OneDrive/Desktop/Project-LAML/config/config.R")
+source("config/config.R")
 
 library(maftools)
 library(data.table)
