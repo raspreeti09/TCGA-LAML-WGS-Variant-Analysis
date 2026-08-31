cdec68363488740694e15d32bd9f81c63e62d00e
Ras Preeti Sharma <raspreeti09@gmail.com>
2026-08-31 13:19:49 +0530

    Update 03_data_preprocessing.R

--- 03_data_preprocessing.R
+++ 03_data_preprocessing.R
@@ -12,7 +12,7 @@
 #   8. Save filtered MAF and QC metrics
 # =============================================================================
 
-source("c:/Users/asus/OneDrive/Desktop/Project-LAML/config/config.R")
+source("config/config.R")
 
 library(maftools)
 library(data.table)
